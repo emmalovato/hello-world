4b239d3e6518e5b1d4ed125261daaab367d011e5
Emma <emmakpeterson@gmail.com>
2020-08-31 18:22:54 -0700

    first trivia route and components

---
+++ src/components/TriviaRound.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="trivia-round">
+    <h1>{{ round }}</h1>
+    <h3>Question{{ questionNumTest }}</h3>
+    <ul>
+      <p>Question Text</p>
+      <img alt="Question image" :src="imgUrl">
+    </ul>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  
+  name: 'TriviaRound',
+  
+  props: {
+    round: String
+  },
+  
+  data: function () {
+    return {
+      // textUrl: 'https://emma-peterson-trivia.s3-us-west-1.amazonaws.com/round-1/questions/text/q1.txt',
+      imgUrl: 'https://emma-peterson-trivia.s3-us-west-1.amazonaws.com/round-1/questions/images/q1.png',
+      questionNumTest: 1
+    }
+  },
+  
+  //created: function () {
+    // `this` points to the vm instance
+    //console.log('a is: ' + this.a)
+  //}
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+</style>
---
+++ src/views/Trivia.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="trivia">
+    <!-- img alt="Trivia image" src="../assets/trivia.png" -->
+
+      <div id='trivia-image'/>
+
+    <trivia-round round="Test Round"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TriviaRound from '@/components/TriviaRound.vue'
+
+export default {
+  name: 'Trivia',
+  components: {
+    TriviaRound
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#trivia-image {
+  width: 300px;
+  height: 177px;
+  margin: 0 auto;
+  background-size: contain;
+  background-image: url('../assets/trivia.png');
+}
+</style>
